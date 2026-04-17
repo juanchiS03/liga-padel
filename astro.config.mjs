c6b2d88586c36682b11124478739a06b319b34cc
juanchiS03 <juansanchezdecorta@gmail.com>
2026-04-17 14:08:06 +0200

    config inicial

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,5 +1,14 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwindcss from '@tailwindcss/vite';
+
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  srcDir: './frontend/src', 
+  // Le decimos dónde están los archivos públicos (imágenes, etc.)
+  publicDir: './frontend/public', 
+  vite: {
+    plugins: [tailwindcss()]
+  }
+});
